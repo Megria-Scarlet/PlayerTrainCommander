703aafdcf2f78286380d224aef1396ba7dc89cc4
Megria Scarlet <megriascarlet@gmail.com>
2026-08-16 21:14:00 +0900

    Update Station.cs

--- PlayerTrainCommanderCore/PlayerTrainCommanderCore/Station.cs
+++ PlayerTrainCommanderCore/PlayerTrainCommanderCore/Station.cs
@@ -13,15 +13,20 @@ namespace PTC.Core
     public class Station : IInherentObject
     {
         private string id;
+        private string? name;
+        private string? abbreviation;
 
         /// <summary>
         /// 固有の ID と表示名を指定して、新しい <see cref="Station"/> 型のオブジェクトを作成します。
         /// </summary>
         /// <param name="id">固有の ID 。</param>
         /// <param name="name">識別に使用する任意の文字列。</param>
-        public Station(string id, string name)
+        /// <param name="abbreviation">識別に使用する任意の省略文字列。</param>
+        public Station(string id, string? name, string? abbreviation)
         {
             this.id = id;
+            this.name = name;
+            this.abbreviation = abbreviation;
         }
 
         /// <summary>
