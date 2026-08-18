303b98f2a4f86fda6744a56e142d97c42692fda4
Megria Scarlet <megriascarlet@gmail.com>
2026-08-18 22:02:36 +0900

    Update Train.cs

--- PlayerTrainCommanderCore/PlayerTrainCommanderCore/Train.cs
+++ PlayerTrainCommanderCore/PlayerTrainCommanderCore/Train.cs
@@ -1,8 +1,24 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 namespace PTC.Core
 {
     public class Train
     {
         private string id;
         private string? name;
+    }
+    public class TrainJsonConverter : JsonConverter<Train>
+    {
+        public override Train? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(Utf8JsonWriter writer, Train value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
