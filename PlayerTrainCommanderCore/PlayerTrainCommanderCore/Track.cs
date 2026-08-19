a31a07ec06920831305b4a01cb1cd8ded1b8e3a1
Megria Scarlet <megriascarlet@gmail.com>
2026-08-20 03:00:58 +0900

    Update Track.cs

--- PlayerTrainCommanderCore/PlayerTrainCommanderCore/Track.cs
+++ PlayerTrainCommanderCore/PlayerTrainCommanderCore/Track.cs
@@ -11,5 +11,13 @@ namespace PTC.Core
     /// </summary>
     public class Track
     {
+        /// <summary>
+        /// 上り方面と下り方面の接続先を管理する基本的なクラス。
+        /// </summary>
+        public class BaseLinker
+        {
+            protected string[]? upIds;
+            protected string[]? downIds;
+        }
     }
 }
