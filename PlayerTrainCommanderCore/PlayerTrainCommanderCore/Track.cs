e2b20ce8e13817d8b07be744dd8872710b5492ec
Megria Scarlet <megriascarlet@gmail.com>
2026-08-20 03:08:49 +0900

    Update Track.cs

--- PlayerTrainCommanderCore/PlayerTrainCommanderCore/Track.cs
+++ PlayerTrainCommanderCore/PlayerTrainCommanderCore/Track.cs
@@ -14,10 +14,52 @@ namespace PTC.Core
         /// <summary>
         /// 上り方面と下り方面の接続先を管理する基本的なクラス。
         /// </summary>
-        public class BaseLinker
+        public struct Linker
         {
-            protected string[]? upIds;
-            protected string[]? downIds;
+            string[]? upIds;
+            string[]? downIds;
+
+            #region コンストラクタ
+            /// <summary>
+            /// 上り方面と下り方面の接続先を示す文字列型の配列を指定して、新しい <see cref="Linker"/> 型のオブジェクトを作成します。
+            /// </summary>
+            /// <param name="upIds">上り方面の接続先を示す文字列型の配列。</param>
+            /// <param name="downIds">下り方面の接続先を示す文字列型の配列。</param>
+            /// <remarks>
+            /// 指定した配列をシャローコピーした配列を内部で使用します。
+            /// </remarks>
+            public Linker(string[]? upIds, string[]? downIds)
+            {
+                if (upIds is not null)
+                    this.upIds = (string[])upIds.Clone();
+                if (downIds is not null)
+                    this.downIds = (string[])downIds.Clone();
+            }
+            /// <summary>
+            /// 上り方面と下り方面の接続先を示す文字列型のコレクションを指定して、新しい <see cref="Linker"/> 型のオブジェクトを作成します。
+            /// </summary>
+            /// <param name="upIds">上り方面の接続先を示す文字列型のコレクション。</param>
+            /// <param name="downIds">下り方面の接続先を示す文字列型のコレクション。</param>
+            public Linker(IEnumerable<string>? upIds, IEnumerable<string>? downIds)
+            {
+                if (upIds is not null && upIds.Any())
+                    this.upIds = [.. upIds];
+                if (downIds is not null && downIds.Any())
+                    this.downIds = [.. downIds];
+            }
+            /// <summary>
+            /// 上り方面と下り方面の接続先を示す文字列型のスパンを指定して、新しい <see cref="Linker"/> 型のオブジェクトを作成します。
+            /// </summary>
+            /// <param name="upIds">上り方面の接続先を示す文字列型のスパン。</param>
+            /// <param name="downIds">下り方面の接続先を示す文字列型のスパン。</param>
+            public Linker(scoped ReadOnlySpan<string> upIds, scoped ReadOnlySpan<string> downIds)
+            {
+                if (!upIds.IsEmpty)
+                    this.upIds = upIds.ToArray();
+                if (!downIds.IsEmpty)
+                    this.downIds = downIds.ToArray();
+            }
+            #endregion
         }
     }
 }
