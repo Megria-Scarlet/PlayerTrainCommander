46e5b45be3cbcb28cffb31074689f7de5a25e898
Megria Scarlet <megriascarlet@gmail.com>
2026-08-20 05:03:57 +0900

    Update TrackFile.cs

--- PlayerTrainCommanderCore/PlayerTrainCommanderCore/Loader/TrackFile.cs
+++ PlayerTrainCommanderCore/PlayerTrainCommanderCore/Loader/TrackFile.cs
@@ -16,6 +16,24 @@ namespace PTC.Core.Loader
             this.version = version;
             this.tracks = tracks;
         }
+        /// <summary>
+        /// <see cref="Track"/> 型のオブジェクトを取得します。
+        /// </summary>
+        /// <returns>読み取られた <see cref="Track"/> 型のオブジェクト。</returns>
+        public ReadOnlySpan<Track> Tracks
+        {
+            [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+            get => tracks;
+        }
+        /// <summary>
+        /// バージョンを示す文字列を取得します。
+        /// </summary>
+        /// <returns>バージョンを示す文字列。</returns>
+        public string Version
+        {
+            [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+            get => version;
+        }
     }
 #pragma warning restore CS1591 // 公開されている型またはメンバーの XML コメントがありません
 }
