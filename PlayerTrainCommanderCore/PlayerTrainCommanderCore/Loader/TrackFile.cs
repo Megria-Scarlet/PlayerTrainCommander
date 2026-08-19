6b8c9ac9d1f36ebd1d8c7e98894906b2b57be083
Megria Scarlet <megriascarlet@gmail.com>
2026-08-20 05:03:12 +0900

    Create TrackFile.cs

---
+++ PlayerTrainCommanderCore/PlayerTrainCommanderCore/Loader/TrackFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTC.Core.Loader
+{
+#pragma warning disable CS1591 // 公開されている型またはメンバーの XML コメントがありません
+    public class TrackFile
+    {
+        private string version;
+        private Track[] tracks;
+        public TrackFile(string version, Track[] tracks)
+        {
+            this.version = version;
+            this.tracks = tracks;
+        }
+    }
+#pragma warning restore CS1591 // 公開されている型またはメンバーの XML コメントがありません
+}
