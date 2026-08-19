5056d8bab03607409e4f5a6c73d16cc98852f4a4
Megria Scarlet <megriascarlet@gmail.com>
2026-08-20 05:28:59 +0900

    Create CoreLoader.cs

---
+++ PlayerTrainCommanderCore/PlayerTrainCommanderCore/Loader/CoreLoader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTC.Core.Loader
+{
+    public class CoreLoader
+    {
+        private DirectoryInfo rootDirectory;
+
+        public CoreLoader(DirectoryInfo rootDirectory)
+        {
+            this.rootDirectory = rootDirectory;
+        }
+
+        public void Load(ILoaderCallback callback)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                callback.WriteError("An exception occurred.", ex);
+            }
+        }
+    }
+}
