5a28ff213d2c2c1e91b33b0f144cdcc6c85785b8
Megria Scarlet <megriascarlet@gmail.com>
2026-08-20 05:13:43 +0900

    Update TrackFile.cs

--- PlayerTrainCommanderCore/PlayerTrainCommanderCore/Loader/TrackFile.cs
+++ PlayerTrainCommanderCore/PlayerTrainCommanderCore/Loader/TrackFile.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace PTC.Core.Loader
 {
 #pragma warning disable CS1591 // 公開されている型またはメンバーの XML コメントがありません
+    [JsonConverter(typeof(TrackFileJsonConverter))]
     public class TrackFile
     {
         private string version;
@@ -33,6 +36,84 @@ namespace PTC.Core.Loader
         {
             [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
             get => version;
+        }
+        /// <summary>
+        /// json ファイルのストリームからデータを読み取り、 <see cref="TrackFile"/> 型のオブジェクトを取得します。
+        /// </summary>
+        /// <param name="utf8Json">json ファイルのストリーム。</param>
+        /// <returns>json ファイルから読み取られた <see cref="TrackFile"/> 型のオブジェクト。</returns>
+        /// <param name="options">デシリアライズに使用するオプション。</param>
+        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+        public static TrackFile? FromJson(System.IO.Stream utf8Json, JsonSerializerOptions? options = null)
+        {
+            return JsonSerializer.Deserialize<TrackFile>(utf8Json, options);
+        }
+    }
+    public class TrackFileJsonConverter : JsonConverter<TrackFile>
+    {
+        public override TrackFile? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (typeof(TrackFile).IsAssignableFrom(typeToConvert))
+            {
+                if (reader.TokenType == JsonTokenType.StartObject)
+                {
+                    string? ver = null;
+                    int startDepth = reader.CurrentDepth;
+                    List<Track> tracks = new List<Track>();
+
+                    while (reader.Read())
+                    {
+                        if (reader.TokenType == JsonTokenType.EndObject)
+                        {
+                            if (startDepth <= reader.CurrentDepth)
+                                break;
+                        }
+                        else if (reader.TokenType == JsonTokenType.PropertyName)
+                        {
+                            if (reader.ValueTextEquals("version"))
+                            {
+                                reader.Read();
+                                ver = reader.GetString();
+                            }
+                            else if (reader.ValueTextEquals("data"))
+                            {
+                                reader.Read();
+                                if (reader.TokenType == JsonTokenType.StartArray)
+                                {
+                                    reader.Read();
+                                    do
+                                    {
+                                        Track? track = JsonSerializer.Deserialize<Track>(ref reader, options);
+                                        if (track is not null)
+                                        {
+                                            if (string.Equals(track.Id, "Switch", StringComparison.InvariantCultureIgnoreCase))
+                                            {
+                                                tracks.Add(new BranchingTrack(track.Id, track.Length, track.SpeedLimit, track.Link));
+                                            }
+                                            else
+                                            {
+                                                tracks.Add(track);
+                                            }
+                                        }
+                                        reader.Read();
+                                    }
+                                    while (reader.TokenType != JsonTokenType.EndArray);
+                                }
+                            }
+                        }
+                    }
+                    if (ver is not null)
+                        return new TrackFile(ver, [.. tracks]);
+
+                    _ = ver;
+                }
+            }
+            return null;
+        }
+
+        public override void Write(Utf8JsonWriter writer, TrackFile value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
         }
     }
 #pragma warning restore CS1591 // 公開されている型またはメンバーの XML コメントがありません
