9d07d4136b5be525c31229c9d8f3291fc66ce2c0
Megria Scarlet <megriascarlet@gmail.com>
2026-08-21 11:19:53 +0900

    Update TrackJsonConverter.cs

--- PlayerTrainCommanderCore/PlayerTrainCommanderCore/Loader/TrackJsonConverter.cs
+++ PlayerTrainCommanderCore/PlayerTrainCommanderCore/Loader/TrackJsonConverter.cs
@@ -16,72 +16,8 @@ namespace PTC.Core
             {
                 if (reader.TokenType == JsonTokenType.StartObject)
                 {
-                    string? id = null;
-                    string? trackType = null;
-                    uint length = 0;
-                    uint speedLimit = 0;
-                    Track.Linker linker = default;
-
-
-                    int startDepth = reader.CurrentDepth;
-                    while (reader.Read())
-                    {
-                        if (reader.TokenType == JsonTokenType.PropertyName)
-                        {
-                            if (reader.ValueTextEquals("trackid"))
-                            {
-                                reader.Read();
-                                id = reader.GetString();
-                                if (id is null)
-                                    throw new JsonException("ID を null に設定することはできません。");
-                            }
-                            else if (reader.ValueTextEquals("tracktype"))
-                            {
-                                reader.Read();
-                                trackType = reader.GetString();
-                            }
-                            else if (reader.ValueTextEquals("length"))
-                            {
-                                reader.Read();
-                                length = reader.GetUInt32();
-                            }
-                            else if (reader.ValueTextEquals("speedlimit"))
-                            {
-                                reader.Read();
-                                speedLimit = reader.GetUInt32();
-                            }
-                            else if (reader.ValueTextEquals("joins"))
-                            {
-                                reader.Read();
-                                if (reader.TokenType == JsonTokenType.StartObject)
-                                {
-                                    linker = JsonSerializer.Deserialize<Track.Linker>(ref reader, options);
-                                    reader.Read();
-                                }
-                                else
-                                {
-                                    reader.Skip();
-                                }
-                            }
-                            else
-                            {
-                                reader.Skip();
-                            }
-                        }
-                        else if (reader.TokenType == JsonTokenType.EndObject)
-                        {
-                            if (reader.CurrentDepth <= startDepth)
-                                break;
-                        }
-                        else
-                        {
-                            reader.Skip();
-                        }
-                    }
-                    if (id is not null)
-                        return new Track(id, trackType ?? "Normal", length, speedLimit, linker);
-                    else
-                        throw new JsonException("プロパティ \"id\" が見つかりませんでした。");
+                    JsonElement element = JsonElement.ParseValue(ref reader);
+                    return Read100(element, options);
                 }
             }
             return null;
@@ -91,6 +27,79 @@ namespace PTC.Core
         public override void Write(Utf8JsonWriter writer, Track value, JsonSerializerOptions options)
         {
             throw new NotImplementedException();
+        }
+
+        private Track Read100(in JsonElement element, JsonSerializerOptions options)
+        {
+            string? id = null;
+            if (element.TryGetProperty("trackid", out JsonElement element1))
+            {
+                id = element1.GetString() ?? throw new NullReferenceException("ID を \"null\" に設定することはできません。");
+            }
+            else
+            {
+                throw new System.Collections.Generic.KeyNotFoundException("\"trackid\" プロパティが存在しません。");
+            }
+            const string Normal = "Normal";
+            string? trackType;
+            if (element.TryGetProperty("tracktype", out element1))
+            {
+                trackType = element1.GetString() ?? Normal;
+            }
+            else
+            {
+                trackType = Normal;
+            }
+            uint length;
+            if (element.TryGetProperty("length", out element1))
+            {
+                length = element1.GetUInt32();
+            }
+            else
+            {
+                length = 0;
+            }
+            uint speedLimit;
+            if (element.TryGetProperty("speedlimit", out element1))
+            {
+                speedLimit = element1.GetUInt32();
+            }
+            else
+            {
+                speedLimit = 0;
+            }
+            Track.Linker linker;
+            if (element.TryGetProperty("joins", out element1))
+            {
+                linker = JsonSerializer.Deserialize<Track.Linker>(element1, options);
+            }
+            else
+            {
+                linker = default;
+            }
+            return ConvertTrack(in id, in trackType, length, speedLimit, in linker, in element, options);
+        }
+        /// <summary>
+        /// <paramref name="trackType"/> に応じた <see cref="Track"/> 型を継承するオブジェクトを取得します。
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="trackType"></param>
+        /// <param name="length"></param>
+        /// <param name="speedLimit"></param>
+        /// <param name="linker"></param>
+        /// <param name="element"></param>
+        /// <param name="options"></param>
+        /// <returns><see cref="Track"/> 型を継承するオブジェクト。</returns>
+        protected virtual Track ConvertTrack(in string id, in string trackType, uint length, uint speedLimit, in Track.Linker linker, in JsonElement element, JsonSerializerOptions options)
+        {
+            if (string.Equals(trackType, "Switch"))
+            {
+                return new BranchingTrack(id, length, speedLimit, linker);
+            }
+            else
+            {
+                return new Track(id, trackType, length, speedLimit, linker);
+            }
         }
     }
 }
