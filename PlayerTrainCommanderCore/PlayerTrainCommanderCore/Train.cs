35b2f1409bcfece0f815a2d34327dabe150519d3
Megria Scarlet <megriascarlet@gmail.com>
2026-08-21 11:58:14 +0900

    Update Train.cs

--- PlayerTrainCommanderCore/PlayerTrainCommanderCore/Train.cs
+++ PlayerTrainCommanderCore/PlayerTrainCommanderCore/Train.cs
@@ -13,6 +13,16 @@ namespace PTC.Core
     /// </summary>
     public class Train : INotifyPropertyChanged
     {
+        #region 公開定数
+        /// <summary>
+        /// 上り方面を示す定数。
+        /// </summary>
+        public int DirectionUp = 1;
+        /// <summary>
+        /// 下り方面を示す定数。
+        /// </summary>
+        public int DirectionDown = -1;
+        #endregion
         #region フィールド変数
         /// <summary>
         /// 現在の列車状態。
@@ -22,6 +32,19 @@ namespace PTC.Core
         /// 列車情報。
         /// </summary>
         protected TrainData trainData;
+        /// <summary>
+        /// 現在の閉そく。
+        /// </summary>
+        protected Track currentTrack;
+        /// <summary>
+        /// 進行方向。
+        /// </summary>
+        /// <value>
+        /// <b>-1</b> 下り方面<br></br>
+        /// <b>0</b> なし<br></br>
+        /// <b>1</b> 上り方面
+        /// </value>
+        protected int direction;
         /// <summary>
         /// 同期処理に使用する <see cref="System.Threading.SpinLock"/> 構造体。
         /// </summary>
@@ -71,26 +94,64 @@ namespace PTC.Core
                 return trainData;
             }
         }
+        /// <summary>
+        /// 進行方向を示す値を取得します。
+        /// </summary>
+        /// <returns>
+        /// 進行方向を示す 32 ビット符号付き整数。
+        /// </returns>
+        /// <remarks>
+        /// このメソッドはスレッドセーフです。
+        /// </remarks>
+        /// <value>
+        /// <b>-1</b> 下り方面<br></br>
+        /// <b>0</b> なし<br></br>
+        /// <b>1</b> 上り方面
+        /// </value>
+        public int Direction
+        {
+            get
+            {
+                bool gotLock = false;
+                try
+                {
+                    spinLock.Enter(ref gotLock);
+                    return direction;
+                }
+                finally
+                {
+                    if (gotLock) spinLock.Exit();
+                }
+            }
+        }
 
         #endregion
+
+        #region コンストラクタ
 
         /// <summary>
         /// 既定の値を使用して、新しい <see cref="Train"/> 型のオブジェクトを作成します。
         /// </summary>
-        public Train() : this(null!)
+        public Train() : this(null!, null!)
         {
 
         }
         /// <summary>
-        /// 列車情報を指定して、新しい <see cref="Train"/> 型のオブジェクトを作成します。
+        /// 列車情報と現在の閉そくを指定して、新しい <see cref="Train"/> 型のオブジェクトを作成します。
         /// </summary>
         /// <param name="trainData">列車情報を示す <see cref="TrainData"/> 型のオブジェクト。</param>
-        public Train(TrainData trainData)
+        /// <param name="currentTrack">現在の閉そくを示す <see cref="Track"/> 型のオブジェクト。</param>
+        /// <param name="direction">進行方向を示す値。</param>
+        public Train(TrainData trainData, Track currentTrack, int direction = 0)
         {
             this.state = TrainState.Undefined;
             this.trainData = trainData;
+            this.currentTrack = currentTrack;
+            this.direction = direction;
             spinLock = new();
         }
+
+        #endregion
 
         #region INotifyPropertyChanged メソッド
         /// <inheritdoc/>
