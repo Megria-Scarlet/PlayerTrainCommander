eaa8a88f1808434aaf8657c8ac0d9e40bf69a259
Megria Scarlet <megriascarlet@gmail.com>
2026-08-21 11:33:45 +0900

    Create TrainState.cs

---
+++ PlayerTrainCommanderCore/PlayerTrainCommanderCore/TrainState.cs
@@ -0,0 +1,38 @@
+﻿namespace PTC.Core
+{
+    /// <summary>
+    /// 列車の状態を表す定数を定義します。
+    /// </summary>
+    public enum TrainState
+    {
+        /// <summary>
+        /// 未定義の値。
+        /// </summary>
+        Undefined = 0,
+        /// <summary>
+        /// 留置中の値。
+        /// </summary>
+        Detention = 1,
+        /// <summary>
+        /// 停車中の値。
+        /// </summary>
+        Stop = 2,
+        /// <summary>
+        /// 乗降中の値。
+        /// </summary>
+        BoardingAndAlighting = 3,
+
+        /// <summary>
+        /// 加速中の値。
+        /// </summary>
+        Acceleration = 128,
+        /// <summary>
+        /// 惰行中の値。
+        /// </summary>
+        Coasting = 129,
+        /// <summary>
+        /// 減速中の値。
+        /// </summary>
+        Deceleration = 130
+    }
+}
