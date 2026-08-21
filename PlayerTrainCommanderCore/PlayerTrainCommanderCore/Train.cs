0c31bff3c272ea58a99ec1d7941182f0f8bfe2b3
Megria Scarlet <megriascarlet@gmail.com>
2026-08-21 11:25:00 +0900

    Create Train.cs

---
+++ PlayerTrainCommanderCore/PlayerTrainCommanderCore/Train.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTC.Core
+{
+    /// <summary>
+    /// 動的に情報が変化する列車を管理するクラス。
+    /// </summary>
+    public class Train : INotifyPropertyChanged
+    {
+        /// <inheritdoc/>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        // This method is called by the Set accessor of each property.
+        // The CallerMemberName attribute that is applied to the optional propertyName
+        // parameter causes the property name of the caller to be substituted as an argument.
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
